bff6ab9f6a4676dcfd7be97960ef90a71caf890a
Archive <137566282+HanazonoFolder@users.noreply.github.com>
2023-06-26 20:08:04 +0800

    Add files via upload

--- trailblaze_cal.cpp
+++ trailblaze_cal.cpp
@@ -20,49 +20,68 @@ int answer;
 int converter;
 
 int main() {
-    std::cout << "Trailblaze Power Left: ";
-    std::cin >> trailblaze_power_left;
 
-    if (trailblaze_power_left < 180) {
-        solution = trailblaze_power_maximum - trailblaze_power_left;
-        answer = solution * trailblaze_power_cooldown;
+    while (true) {
 
-        int hours = std::abs(answer) / 3600; // Get the number of hours
-        int minutes = (std::abs(answer) % 3600) / 60; // Get the number of minutes
-        int seconds = (std::abs(answer) % 3600) % 60; // Get the number of seconds
+        std::cout << endl << "Trailblaze Power Left: ";
+        std::cin >> trailblaze_power_left;
 
-        std::cout << endl << "Will take: "<< std::setw(2) << std::setfill('0') << hours << ":" << std::setw(2) << std::setfill('0') << minutes << ":" << std::setw(2) << std::setfill('0') << seconds << endl;
+        if (trailblaze_power_left < 180) {
+            solution = trailblaze_power_maximum - trailblaze_power_left;
+            answer = solution * trailblaze_power_cooldown;
+
+            int hours = std::abs(answer) / 3600; // Get the number of hours
+            int minutes = (std::abs(answer) % 3600) / 60; // Get the number of minutes
+            int seconds = (std::abs(answer) % 3600) % 60; // Get the number of seconds
+
+            std::cout << endl << "Will take: "<< std::setw(2) << std::setfill('0') << hours << ":" << std::setw(2) << std::setfill('0') << minutes << ":" << std::setw(2) << std::setfill('0') << seconds << endl;
         
-        // Get the current time
-        auto currentTime = std::chrono::system_clock::now();
-        std::time_t currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
-        std::tm* localTime = std::localtime(&currentTime_t);
+            // Get the current time
+            auto currentTime = std::chrono::system_clock::now();
+            std::time_t currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
+            std::tm* localTime = std::localtime(&currentTime_t);
 
-        // Calculate the future time
-        auto futureTime = currentTime + std::chrono::seconds(answer);
-        std::time_t futureTime_t = std::chrono::system_clock::to_time_t(futureTime);
-        std::tm* futureLocalTime = std::localtime(&futureTime_t);
+            // Calculate the future time
+            auto futureTime = currentTime + std::chrono::seconds(answer);
+            std::time_t futureTime_t = std::chrono::system_clock::to_time_t(futureTime);
+            std::tm* futureLocalTime = std::localtime(&futureTime_t);
 
-        // Display the current time
-        std::cout << endl << "Full in: " << std::put_time(localTime, "%I:%M:%S %p") << std::endl;
+            // Display the current time
+            std::cout << endl << "Full in: " << std::put_time(localTime, "%I:%M:%S %p") << std::endl;
 
-        // Get the updated current time
-        currentTime = std::chrono::system_clock::now();
-        currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
-        localTime = std::localtime(&currentTime_t);
+            // Get the updated current time
+            currentTime = std::chrono::system_clock::now();
+            currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
+            localTime = std::localtime(&currentTime_t);
 
-        // Display the updated current time
-        //std::cout << "Updated current time: " << std::put_time(localTime, "%I:%M:%S %p") << std::endl;
+            // Display the updated current time
+            //std::cout << "Updated current time: " << std::put_time(localTime, "%I:%M:%S %p") << std::endl;
 
-        // Display the future time
-        std::cout << endl << "Current time: " << std::put_time(futureLocalTime, "%I:%M:%S %p") << std::endl;
+            // Display the future time
+            std::cout << endl << "Current time: " << std::put_time(futureLocalTime, "%I:%M:%S %p") << std::endl;
+            
+        }
+        else {
+            std::cout << "Invalid Way Above the 180" << endl;
+            return 1;
+        }
         
-        std::cout << endl << endl << "Program will close in 10sec" << endl;
-        std::this_thread::sleep_for(std::chrono::seconds(10));
-    }
-    else {
-        std::cout << "Invalid Way Above the 180" << endl;
-        return 1;
+
+        std::cout << endl << "Would you like to continue? Yes or No?" << endl << endl;
+        string choices;
+        std::cin >> choices;
+
+        if (choices == "yes" || choices == "Yes" || choices == "y") {
+            std::cout << endl << "You wish to continue" << endl << endl;
+        }
+        else if (choices == "no" || choices == "No" || choices == "n") {
+            std::cout << endl << "You wish to stopped" << endl << endl;
+            break;
+            return 1;
+        }
+        else {
+            std::cout << endl << "[Error] Looks like you put something other than yes and no." << endl;
+        }
     }
     return 0;
 }
